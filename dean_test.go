44f17eca7fc7036469b62f0ff36e11e63c008cec
Dean Martin <dmartin@dmsgroup.com>
2023-05-23 12:16:09 -0400

    foo

--- dean_test.go
+++ dean_test.go
@@ -14,7 +14,7 @@ func decorate(stage *evaluationStage) {
 func TestDecoratorCollector(t *testing.T) {
 	log.SetFlags(log.Lshortfile)
 
-	exp, err := NewEvaluableExpression("foo > bar")
+	exp, err := NewEvaluableExpression(`age >= 21 && age <= 24 && state IN ("GA", "FL")`)
 	if err != nil {
 		t.Error("couldn't parse expression: ", err)
 	}
@@ -23,8 +23,8 @@ func TestDecoratorCollector(t *testing.T) {
 	decorate(exp.evaluationStages)
 
 	params := make(MapParameters)
-	params["foo"] = 1
-	params["bar"] = 2
+	params["age"] = 21
+	params["state"] = "GA"
 	res, err := exp.Eval(params)
 
 	if err != nil {
